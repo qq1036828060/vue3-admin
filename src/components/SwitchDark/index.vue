d1f2c97bcdbf1e8a8346eecdc28c6cd9c8940a33
zzg_dream@sina.com <zzg_dream@sina.com>
2023-07-21 22:02:39 +0800

    组件？

---
+++ src/components/SwitchDark/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-switch v-model="globalStore.isDark" inline-prompt :active-icon="Sunny" :inactive-icon="Moon" @change="switchDark" />
+</template>
+
+<script setup lang="ts" name="SwitchDark">
+import { useTheme } from "@/utils/hooks/useTheme";
+import { useGlobalStore } from "@/store/modules/global";
+import { Sunny, Moon } from "@element-plus/icons-vue";
+
+const { switchDark } = useTheme();
+const globalStore = useGlobalStore();
+</script>
